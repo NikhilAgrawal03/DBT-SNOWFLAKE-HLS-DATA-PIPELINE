f4d80b081f79bd2f51c5761f47e06a15af7607c4
Nikhil Ag <secretscorpion03@gmail.com>
2026-02-25 18:15:22 +0000

    time spine semantic models updated

--- models/marts/utility_spines/time_spine.sql
+++ models/marts/utility_spines/time_spine.sql
@@ -2,13 +2,12 @@
 
 with date_spine as (
     {{ dbt_utils.date_spine(
-        start_date="'1970-01-01'",
-        end_date="current_date",
-        datepart="day"
+        datepart="day",
+        start_date="DATE('2000-01-01')",
+        end_date="current_date"
     ) }}
 )
-
-select
-    date_day as date_day
+select cast(date_day as date) as date_day
 from date_spine
-order by date_day
+order by 1;
+``
